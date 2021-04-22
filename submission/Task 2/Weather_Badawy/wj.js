00f78b9f5fc5f17343b984469877dee1cc8bd42e
MBadawy780 <73402597+MBadawy780@users.noreply.github.com>
2021-04-22 22:31:43 +0200

    Update wj.js

--- submission/Task 2/Weather_Badawy/wj.js
+++ submission/Task 2/Weather_Badawy/wj.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const History = require('./models/history.js');
 
-//express app [express is framework for nodejs]
+
 
 const app = express();
 
@@ -25,13 +25,13 @@ mongoose.connect(dbURI, { useNewUrlParser: true,  useUnifiedTopology: true })
 app.use(express.static('views'));
 app.use(express.urlencoded({ extended : true }));
 
-//listen to port 3000
 
-//app.listen(3000);
+
+
 
 app.get('/', (req, res) => {
 
-    //res.send('<h1>Hello World</h1>');
+
     History.find()
       .then((result) => {
         res.render('index', { history : result });
@@ -39,68 +39,22 @@ app.get('/', (req, res) => {
       .catch((err) => {
           console.log(err);
       })
-    // const history = new History({
-
-    //     city : 'aswan',
-
-    //     feeling : 'cool'
-    // })
-    // history.save()
-    // .then((result) => {
-    //     res.send(result);
-    // })
-    // .catch((err) => {
-    //     console.log(err);
-    // })
-   // res.sendFile('./Task4/index.html', {root : __dirname});//express by defult read the path from the core of the computer so to make it read from this file type root..
-   //  res.sendFile(__dirname + '/views/index.ejs');
-    //res.render('index', {history});
-
-    
+   
 
 });
 
-// app.get('/history', (req, res) => {
-
-//     const history = new History({
-
-//         city : req.city,
-
-//         feeling : req.feeling
-//     })
-//     history.save()
-//      .then((result) => {
-//          res.send(result);
-//      })
-//      .catch((err) => {
-//          console.log(err);
-//      })
-// });
-
-// app.get('/all-history', (req, res) => {
-//     History.find()
-//      .then((result) => {
-//          res.send(result);
-//      })
-//      .catch((err) => {
-//          console.log(err);
-//      })
-// });
 
 app.post('/posthistory', (req, res) => {
 
 
-    const history = new History(req.body); //.body to get the entire data in the request
-
+    const history = new History(req.body); 
     history.save()
-      //.then((result) => {
-          //res.redirect('/')
-      //})
+     
      .catch((err) => {
          console.log(err);
      })
 
-  //  console.log(req.body);
+ 
 
 });
 
